e15e0f5e035d592b49a9ca78daae86b7079268d1
AnthonyNaumenko <student.dnu.an@gmail.com>
2018-04-24 03:31:44 +0300

    Homework Добавлено поле к админкам в категориях. сделал вывод товаров на странице категорий

--- src/Admin/CategoryAmdin.php
+++ src/Admin/CategoryAmdin.php
@@ -14,6 +14,7 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class CategoryAmdin extends AbstractAdmin
 {
@@ -21,8 +22,9 @@ class CategoryAmdin extends AbstractAdmin
     {
         $form
             ->add('name')
-            ->add('parent');
-
+            ->add('parent')
+            ->add('imageFile', VichImageType::class, ['required' => false])
+            ;
     }
     protected function configureListFields(ListMapper $list)
 {
--- src/Migrations/Version20180423234733.php
+++ src/Migrations/Version20180423234733.php
@@ -8,17 +8,15 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20180410160844 extends AbstractMigration
+class Version20180423234733 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE order_items (id INT AUTO_INCREMENT NOT NULL, order_id INT DEFAULT NULL, product VARCHAR(255) NOT NULL, quantity_of_ordered_items NUMERIC(2, 0) NOT NULL, price NUMERIC(10, 2) DEFAULT NULL, value NUMERIC(10, 2) DEFAULT NULL, INDEX IDX_62809DB08D9F6D38 (order_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE order_items ADD CONSTRAINT FK_62809DB08D9F6D38 FOREIGN KEY (order_id) REFERENCES orders (id)');
         $this->addSql('DROP TABLE order_item');
-        $this->addSql('ALTER TABLE products CHANGE is_top is_top TINYINT(1) DEFAULT \'0\' NOT NULL');
+        $this->addSql('ALTER TABLE category ADD image_name VARCHAR(255) DEFAULT NULL, ADD image_size INT DEFAULT NULL, ADD image_type VARCHAR(255) DEFAULT NULL, ADD updated_at DATETIME DEFAULT NULL');
     }
 
     public function down(Schema $schema)
@@ -28,7 +26,6 @@ class Version20180410160844 extends AbstractMigration
 
         $this->addSql('CREATE TABLE order_item (id INT AUTO_INCREMENT NOT NULL, order_id INT DEFAULT NULL, product VARCHAR(255) NOT NULL COLLATE utf8mb4_unicode_ci, quantity_of_ordered_items NUMERIC(2, 0) NOT NULL, price NUMERIC(10, 2) DEFAULT NULL, value NUMERIC(10, 2) DEFAULT NULL, INDEX IDX_52EA1F098D9F6D38 (order_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE = InnoDB');
         $this->addSql('ALTER TABLE order_item ADD CONSTRAINT FK_52EA1F098D9F6D38 FOREIGN KEY (order_id) REFERENCES orders (id)');
-        $this->addSql('DROP TABLE order_items');
-        $this->addSql('ALTER TABLE products CHANGE is_top is_top TINYINT(1) DEFAULT NULL');
+        $this->addSql('ALTER TABLE category DROP image_name, DROP image_size, DROP image_type, DROP updated_at');
     }
 }
