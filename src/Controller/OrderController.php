838d7675e9d791551a96f5814bfdb3ebc804a714
AnthonyNaumenko <student.dnu.an@gmail.com>
2018-05-01 17:54:03 +0300

    Homework25. Вывод таблицы(корзина)

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -24,4 +24,30 @@ class OrderController extends Controller
 
         return $this->redirect($request->headers->get('referer','/'));
     }
+
+    /**
+     * @param Orders $ordersCart
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @Route("/orders", name="order_cart")
+     */
+    public function cart(Orders $orders)
+    {
+        //$orders->getCart();
+        $cartId = $orders->getCart()->getId();
+        $cartCreated = $orders->getCart()->getCreatedAt();
+        $status = $orders->getCart()->getStatus();
+        $userId = $orders->getCart()->getUser();
+        $isPaid = $orders->getCart()->getIsPaid();
+        $amount = $orders->getCart()->getAmount();
+
+        return $this->render('order/cart.html.twig',[
+              'cartId'=>$cartId,
+              'cartCreated'=>$cartCreated,
+              'status'=>$status,
+              'user'=>$userId,
+              'isPaid'=>$isPaid,
+              'amount'=>$amount
+
+    ]);
+    }
 }
