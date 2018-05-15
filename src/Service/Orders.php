0cf48557bb41d508a2fe301c90243cf7c7184d40
Anthony <student.dnu.an@gmail.com>
2018-05-15 21:45:00 +0300

    Lesson 28 mail

--- src/Service/Orders.php
+++ src/Service/Orders.php
@@ -5,9 +5,13 @@ use App\Entity\OrderItem;
 use App\Entity\Product;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 class Orders
 {
+
+
     const CART_ID = 'cart';
     /**
      * @var SessionInterface
@@ -17,10 +21,37 @@ class Orders
      * @var EntityManagerInterface
      */
     private $em;
-    public function __construct(SessionInterface $session, EntityManagerInterface $em)
+
+    /**
+     * @var Mailer
+     */
+    private  $mailer;
+
+
+    /**
+     * @var string
+     */
+    private $fromEmail;
+
+
+    /**
+     * @var string
+     */
+    private  $ordersEmail;
+
+
+
+    public function __construct(SessionInterface $session,
+                                EntityManagerInterface $em,
+                                Mailer $mailer,
+                                $fromEmail,
+                                $ordersEmail )
     {
         $this->session = $session;
         $this->em = $em;
+        $this->mailer=$mailer;
+        $this->fromEmail = $fromEmail;
+        $this->ordersEmail = $ordersEmail;
     }
     public function hasCart()
     {
@@ -86,4 +117,18 @@ class Orders
 
         return $cart;
     }
+    public function makeOrder(Order $order)
+    {
+        $order->setStatus(Order::STATUS_ORDERED);
+        $this->em->flush();
+        $this->session->remove(self::CART_ID);
+
+        $this->mailer->sendMessage(
+            'order/mail/manager.msg.twig',
+            ['order'=>$order],
+            $this->fromEmail,
+            $this->ordersEmail
+            );
+    }
+
 }
