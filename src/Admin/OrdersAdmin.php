c1ce928e4025360fa6b8341b0fb0d69d368ffceb
AnthonyNaumenko <student.dnu.an@gmail.com>
2018-05-18 00:32:36 +0300

    Homework28 OrdersAdmin

--- src/Admin/OrdersAdmin.php
+++ src/Admin/OrdersAdmin.php
@@ -9,7 +9,57 @@
 namespace App\Admin;
 
 
-class OrdersAdmin
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class OrdersAdmin extends AbstractAdmin
 {
+    protected function configureFormFields(FormMapper $form)
+    {
+       $form
+           ->add('createdAt')
+           ->add('status')
+           ->add('isPaid')
+           ->add('amount')
+           ->add('email')
+           ->add('phone')
+           ->add('comment')
+           ->add('firstName')
+           ->add('lastName')
+           ;
+    }
+
+    protected function configureListFields(ListMapper $list)
+    {
+     $list
+         ->add('createdAt')
+         ->add('status')
+         ->add('isPaid')
+         ->add('amount')
+         ->addIdentifier('email')
+         ->addIdentifier('phone')
+         ->addIdentifier('comment')
+         ->addIdentifier('firstName')
+         ->addIdentifier('lastName')
+         ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $filter)
+    {
+        $filter
+            ->add('status')
+            ->add('isPaid')
+            ->add('amount')
+            ->add('createdAt')
+            ->add('email')
+            ->add('phone')
+            ->add('comment')
+            ->add('firstName')
+            ->add('lastName')
+        ;
+    }
+
 
 }
